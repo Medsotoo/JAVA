203499c2b4e9cbfe00cdd9ef7790b5588206c25a
Sergey Palchikov <example.example1928@gmail.com>
2023-03-08 19:06:29 +0300

    DZ_1 taski

--- practicum.java
+++ practicum.java
@@ -1,6 +1,26 @@
+import java.util.Arrays;
+
 public class practicum {
     public static void main(String[] args){
-        System.out.println("Hello word");
+        int[] array = {3, 5, 5, 6, 7, 8, 97, 5}; 
+        int val = 5;
+        swap(array, val);
+        System.out.println(Arrays.toString(array));
+        
+        
+    }
+
+    static void swap(int[] array, int val){
+        int shift = 1;
+        for (int i = 0; i < array.length / 2; i++){
+            if (array[i] == val){
+                int tmp = array[i];
+                array[i] = array[array.length - shift];
+                array[array.length - shift] = tmp;
+                shift++;
+            }
+        }
+
     }
     
 }
