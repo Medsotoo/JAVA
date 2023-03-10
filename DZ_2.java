2e5426f415d81e11939b0f02d56b86c47f49e0a1
Sergey Palchikov <example.example1928@gmail.com>
2023-03-10 11:01:45 +0300

    task 2 DZ

--- DZ_2.java
+++ DZ_2.java
@@ -1,33 +1,67 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+
+import javax.swing.ProgressMonitorInputStream;
 
 public class DZ_2 {
 
 
     public static void main(String[] args) {
-        int n = 10;
-        int c1 = 3;
-        int c2 = 2;
-        res(n, c1, c2);
+        System.out.println(buldString(10, 'a', 'b'));
+
+
+//         // Получаем путь к нужному файлу
+// Path writeFilePath = Paths.get("/tmp/file.txt");
+
+// // Создаём файл
+// Path createdFilePath = Files.createFile(writeFilePath);
+// System.out.println("Файл создан: " + createdFilePath);
+
+// // Записываем данные в файл
+// String str = "Hello world!";
+// // Строку переводим в двоичный тип данных
+// byte[] strToBytes = str.getBytes();
+
+// Files.write(createdFilePath, strToBytes);
+        // Path of = Paths.get("/JAVA/file.txt"); 
+        // Path creat = Files.createFile(of);
+        // System.out.println("Файл создан: " + creat);
+
+        // Path of = Path.of("file.txt");
+        // Path path = of.toAbsolutePath();
+        // Files.createFile("Tesf.txt", null);
+        // if (Files.exists(path) == false){
+        //     Files.createFile(path);
+        // }
     }
     
 
     // Дано четное число N (>0) и символы c1 и c2.
     // Написать метод, который вернет строку длины N, которая состоит из чередующихся символов c1 и c2, начиная с c1. (6, 'a', 'b') -> "ababab"
-    private static void res(int n, int c1, int c2){ 
-        int[] arr = new int[n];
+    static String buldString(int n, char c1, char c2){ 
+        String res = "";
         for(int i = 0; i < n / 2; i++){
-            arr[i + i] = c1;
-            arr[i * 2 + 1] = c2;
+            res += c1;
+            res += c2;
         }
-        System.out.println(Arrays.toString(arr));
-
+        return res;
+                
     }
-
-
-    //Создать файл с названием file.txt. Если файл уже есть, то создавать не надо Записать в него Слово "TEXT" 100 раз
-
     
 
 
-   
+    //Создать файл с названием file.txt. Если файл уже есть, то создавать не надо Записать в него Слово "TEXT" 100 раз 
+    
+    // public static void file(String[] args) {
+		
+	// 	try {
+	// 		String str = "Some write file Example";
+	// 		byte[] bs = str.getBytes();
+	// 		Path writtenFilePath = Files.write(path, bs);
+	// 		System.out.println("Written content in file:\n"+ new String(Files.readAllBytes(writtenFilePath)));
+	// 	} catch (Exception e) {
+	// 		e.printStackTrace();
+	// 	}
 }
